1024e206ae3b11db67400f537d09697a00ef947a
Lin-14 <1009811744@qq.com>
2021-03-06 19:54:37 +0800

    update project

---
+++ src/components/Login.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <div class="avator_box">
+          <img src="../assets/rukawa.png" alt="">
+      </div>
+      <!-- 登录表单 -->
+      <el-form ref="loginFormRef" :model="loginForm" :rules="loginFormRules" label-width="0px" class="login_form">
+        <!-- 用户 -->
+        <el-form-item prop="username">
+          <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item prop="password">
+          <el-input v-model="loginForm.password" prefix-icon="iconfont icon-3702mima" type="password"></el-input>
+        </el-form-item>
+        <!-- 按钮 -->
+        <el-form-item class="btns">
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="info" @click="resetLoginForm">清空</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      // 登录表单的数据绑定对象
+      loginForm: {
+        username: 'admin',
+        password: '123456'
+      },
+      // 表单的验证规则对象
+      loginFormRules: {
+        // 验证用户名是否合法
+        username: [
+          { required: true, message: '请输入用户名称', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ],
+        // 验证密码是否合法
+        password: [
+          { required: true, message: '请输入登录密码', trigger: 'blur' },
+          { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    // 清空表单
+    resetLoginForm() {
+      this.$refs.loginFormRef.resetFields();
+    },
+    login() {
+      this.$refs.loginFormRef.validate(async valid => {
+        // 用async和await简化处理promise
+        if(!valid)return;
+        const { data: res } = await this.$http.post('login', this.loginForm);
+        if(res.meta.status !== 200) return console.log('登录失败！');
+        console.log('登录成功！');
+        // console.log(res);
+      });
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  .login_container{
+    background-color: #5dbe8a;
+    height: 100%;
+  }
+  .login_box{
+    width: 450px;
+    height: 300px;
+    background-color: #fff;
+    border-radius: 5px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+
+    .avator_box{
+        width: 120px;
+        height: 120px;
+        border: 1px solid #eee;
+        border-radius: 50%;
+        padding: 8px;
+        box-shadow: 0 0 10px #ddd;
+        position: absolute;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        background-color: #fff;
+      img{
+          width: 100%;
+          height: 100%;
+          border-radius: 50%;
+      }
+    }
+
+    .login_form{
+      width: 100%;
+      position: absolute;
+      bottom: 0;
+      padding: 0 35px;
+      box-sizing: border-box;
+    }
+    
+    .btns{
+      display: flex;
+      justify-content: flex-end;
+    }
+  }
+</style>
